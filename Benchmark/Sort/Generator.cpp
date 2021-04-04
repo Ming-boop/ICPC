0a881142d62fd13da1b45aab8d7ff282f687ba4f
coretus <54952921+Ming-boop@users.noreply.github.com>
2021-04-04 10:16:40 +0800

    Update Random Number Generator

---
+++ Benchmark/Sort/Generator.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+const std::string FILE_PREFIX = "Data/Data", FILE_SUFFIX = ".in";
+const int num[] = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, int(1e4), int(2e4), int(5e4), int(1e5), int(2e5), int(5e5), int(1e6), int(2e6), int(5e6), int(1e7), int(2e7)};
+const int MAX_GROUP = 20;
+
+void generate_random(int num, int id)
+{
+    std::string file_name = FILE_PREFIX + std::to_string(id) + FILE_SUFFIX;
+    freopen(file_name.c_str(), "w", stdout);
+    printf("%d\n", num);
+    for (int i = 0; i < num; i++)
+    {
+        if (i)
+        {
+            putchar(' ');
+        }
+        printf("%d", rand());
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    for (int i = 0; i < MAX_GROUP; i++)
+    {
+        generate_random(num[i], i + 1);
+    }
+    return 0;
+}
