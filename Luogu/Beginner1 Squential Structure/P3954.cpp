543abf1ff40618a2e47f9e2ff5b89f2db8e9bef8
coretus <54952921+Ming-boop@users.noreply.github.com>
2021-08-18 15:04:03 +0800

    Update P3954.cpp GPA

---
+++ Luogu/Beginner1 Squential Structure/P3954.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    double gpa = 0.2 * a + 0.3 * b + 0.5 * c;
+    cout << gpa << endl;
+    return 0;
+}
