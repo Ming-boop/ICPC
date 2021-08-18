3bf3ae07245a560528bc6223a46ae01da9bf44ea
coretus <54952921+Ming-boop@users.noreply.github.com>
2021-08-18 15:26:19 +0800

    Update Problem ID

---
+++ Luogu/Beginner1 Squential Structure/P5704.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    cout << char(toupper(a)) << endl;
+    return 0;
+}
